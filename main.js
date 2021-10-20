09433113744feeaa817add3c403631d7dba74cd4
Michael <michael.ziso@taboola.com>
2021-10-20 12:05:33 +0300

    update data handle and ui

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const res = [], res1 = [];
+const consoleData = [], res = [];
 const   areasData = {
   taken: [],
   name: [],
@@ -10,6 +10,8 @@ const sum = {
   counter: 0
 }
 
+const labels = ['שם היציע', 'תכולת כסאות ביציע', 'כמות כסאות פנויים', 'כמה כרטיסים נמכרו'];
+
 function filterGates(gate) {
   return gate > 500
          || gate.indexOf('תא תקשורת') > -1
@@ -18,7 +20,7 @@ function filterGates(gate) {
 }
 
 function handleData(index) {
-  data[index].forEach(area => {
+  data.areas[index].forEach(area => {
     const {capacity, name, free} = area;
     const taken = capacity - free;
 
@@ -26,8 +28,8 @@ function handleData(index) {
       return;
     }
 
-    res.push({name, capacity, free, taken})
-    res1.push([name, capacity, free, taken])
+    consoleData.push({name, capacity, free, taken})
+    res.push([name, capacity, free, taken].reverse())
 
     areasData.taken.push(taken);
     areasData.free.push(free);
@@ -39,69 +41,119 @@ function handleData(index) {
   })
 
   console.table(sum);
-  console.table(res);
+  console.table(consoleData);
 }
 
+function printSummery(){
+  const el = document.querySelector('.data-total');
+  el.insertAdjacentHTML('afterbegin',
+                        `<div class="data-wrp">
+                <div class="title">תכולת כסאות ביציע</div>
+                <h2 class="capacity">${sum.capacity}</h2>
+            </div>
+            <div  class="data-wrp"> 
+                <div class="title">כמה כרטיסים נמכרו</div>
+                <h2 class="taken">${sum.counter}</h2>
+            </div>
+            <div class="data-wrp">
+                <div class="title">כמות כסאות פנויים</div>
+                <h2 class="free">${sum.free}</h2>
+            </div>`)
+}
 
+function isMobile(){
+  return window.matchMedia("only screen and (max-width: 760px)").matches;
+}
 
 function printData(){
-  const {capacity, counter, free} = sum;
+  /*const {capacity, counter, free} = sum;
   new gridjs.Grid({
-                    columns: ['תכולת כסאות ביציע', 'כמה כרטיסים נמכרו', 'כמות כסאות פנויים'],
+                    columns: labels,
                     data: [
                       [capacity, counter, free]
                     ]
-                  }).render(document.getElementById("table-total"));
+                  }).render(document.getElementById("table-total"));*/
 
-/*  new gridjs.Grid({
-                    columns: ['שם היציע', 'תכולת כסאות ביציע', 'כמות כסאות פנויים', 'כמה כרטיסים נמכרו'],
-                    data: res1
-                  }).render(document.getElementById("table-area"));*/
+new gridjs.Grid({
+                    columns: labels.reverse(),
+                    data: res,
+                  sort: true,
+                  search: {
+                    enabled: true
+                  },
+                  pagination: {
+                    enabled: true,
+                    limit: 10
+                  }
+                  }).render(document.getElementById("table-area"));
 }
 
-function printCharts(){
-  const {capacity, counter, free} = sum;
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var ctx2 = document.getElementById('myChart2').getContext('2d');
-  var total = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-      labels: ['תכולת כסאות ביציע', 'כמה כרטיסים נמכרו', 'כמות כסאות פנויים'],
-      datasets: [{
-        label: 'מידע לפי אתר לאן',
-        data: [capacity, counter, free],
-        backgroundColor: [
-          'rgb(16,14,7)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderColor: [
-          'rgb(16,14,7)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(255, 159, 64, 1)'
-        ]
-      }]
-    }
-  });
-
-  var area = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: areasData.name,
-      datasets: [{
-        label: 'כמה כרטיסים נשארו לפי יציע',
-        data: areasData.free,
-        backgroundColor: [
-          'rgba(255, 206, 86, 1)',
-        ],
-      }]
-    }
-  });
+function printCharts(data, container){
+  var ctx = document.getElementById(container).getContext('2d');
+  new Chart(ctx, data);
 }
 
-handleData(0);
-printData();
-printCharts();
+function setHeaderImage(device){
+  const headerEl = document.querySelector('header');
+  headerEl.style.backgroundImage = `url('${data.bg[device]}')`;
+}
+
+function init(){
+  handleData(0);
+  printSummery();
+
+  const device = isMobile() ? 'mobile' : 'desktop';
+  setHeaderImage(device);
+
+  if(device === 'mobile'){
+    printCharts({
+                  type: 'doughnut',
+                  data: {
+                    labels: [ 'כמה כרטיסים נמכרו', 'כמות כסאות פנויים'],
+                    datasets: [{
+                      label: 'מידע לפי אתר לאן',
+                      data: [sum.counter, sum.free],
+                      backgroundColor: [
+                        'rgba(255, 206, 86, 1)',
+                        'rgb(16,14,7)',
+                      ]
+                    }]
+                  }
+                },'total-chart');
+  }else{
+    printData();
+    printCharts({
+                  type: 'bar',
+                  data: {
+                    labels: areasData.name,
+                    datasets: [{
+                      label: 'כמה כרטיסים נשארו לפי יציע',
+                      data: areasData.free,
+                      backgroundColor: [
+                        'rgba(255, 206, 86, 1)',
+                      ],
+                    }]
+                  }
+                },'gate-chart');
+  }
+}
+
+function loadData(){
+  const script = document.createElement('script');
+  script.onload = function () {
+    init();
+  };
+  const searchParams = new URLSearchParams(location.search);
+  const page = searchParams.get('p');
+
+  script.src = `data/${page}.js`;
+
+  document.head.appendChild(script);
+}
+
+loadData();
+
+
 
 
 
