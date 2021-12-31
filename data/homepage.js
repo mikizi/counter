070c1a0cc1165572db80bc39a9c9301b43d99ffa
Michael <michael.ziso@taboola.com>
2021-12-31 14:42:06 +0200

    * Update b-jslm.js * open gate 208 * small date fix on hp

--- data/homepage.js
+++ data/homepage.js
@@ -6,36 +6,36 @@ const data = [
   {
     name: 'm-haifa',
     title: 'מכבי חיפה - מכבי נתניה',
-    date: new Date(2021, 10, 25)
+    date: new Date(2021, 9, 25)
   },
   {
     name: 'h-b7',
     title: 'מכבי נתניה - הפועל באר שבע',
-    date: new Date(2021, 10, 30)
+    date: new Date(2021, 9, 30)
   },
   {
     name: 'm-pt',
     title: 'מכבי נתניה - מכבי פ״ת',
-    date: new Date(2021, 11, 27)
+    date: new Date(2021, 10, 27)
   },
   {
     name: 'ashdod',
     title: 'מכבי נתניה - מ.ס אשדוד',
-    date: new Date(2021, 11, 30)
+    date: new Date(2021, 10, 30)
   },
   {
     name: 'h-tlv',
     title: 'מכבי נתניה - הפועל ת"א',
-    date: new Date(2021, 12, 11)
+    date: new Date(2021, 11, 11)
   },
   {
     name: 'h-haifa',
     title: 'מכבי נתניה - הפועל חיפה',
-    date: new Date(2021, 12, 21)
+    date: new Date(2021, 11, 21)
   },
   {
     name: 'b-jslm',
     title: 'מכבי נתניה - בית"ר ירושלים',
-    date: new Date(2022, 1, 1)
+    date: new Date(2022, 0, 1)
   },
 ];
