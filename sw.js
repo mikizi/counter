ef66bf30a71f96ddd8cc649f5a836efa884eb76a
Michael <michael.ziso@taboola.com>
2022-02-08 16:27:38 +0200

    update cache

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v3';
-const RUNTIME = 'runtime';
+const PRECACHE = 'precache-v4';
+const RUNTIME = 'runtime-v1';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -53,6 +53,9 @@ self.addEventListener('activate', event => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
+  if ( event.request.url.match( '^.*(\/data\/).*$' ) ) {
+    return false;
+  }
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
