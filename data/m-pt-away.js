fc995a10dd70ff5b0f80dd9e604992d84fefdec4
Michael <michael.ziso@taboola.com>
2022-02-19 14:42:26 +0200

    updates

--- data/m-pt-away.js
+++ data/m-pt-away.js
@@ -159,7 +159,7 @@ var data = {
   overrideGateData: {
     '13': {
       capacity: 600,
-      free: 326
+      free: 21
     }
   },
   link: 'https://www.leaan.co.il/he-IL/events/%D7%9E%D7%9B%D7%91%D7%99%20%D7%A4\'%D7%AA%20-%20%D7%9E%D7%9B%D7%91%D7%99%20%D7%A0%D7%AA%D7%A0%D7%99%D7%94/2022-2-19_15.00/%D7%90%D7%A6%D7%98%D7%93%D7%99%D7%95%D7%9F%20%D7%A9%D7%9C%D7%9E%D7%94%20%D7%91%D7%99%D7%98%D7%95%D7%97%20-%20%D7%94%D7%9E%D7%95%D7%A9%D7%91%D7%94?hallmap',
