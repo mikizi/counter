aa09a6474285f1b19963697e47855ac7c00d28a3
michael.ziso <michael.ziso@taboola.com>
2022-07-13 11:11:41 +0300

    fix og image url

--- sw.js
+++ sw.js
@@ -14,8 +14,9 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1.10.9';
-const RUNTIME = 'runtime-v1.10.9';
+const VERSION = '2.1.1'
+const PRECACHE = `precache-v${VERSION}`;
+const RUNTIME = `runtime-v${VERSION}`;
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
