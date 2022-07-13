2595c199407ade520df24fe3dd5743da238149c0
michael.ziso <michael.ziso@taboola.com>
2022-07-13 20:17:52 +0300

    archive brunch

--- data/homepage.js
+++ data/homepage.js
@@ -1,7 +1,146 @@
 const data = [
-   {
-    name: 'toto-m-tlv',
-    title: 'מכבי תל אביב - מכבי נתניה',
-    date: new Date(2022, 6, 14, 20, 0, 0),
+  {
+    name: 'subscription',
+    title: 'מנויים עונה סדירה (2774) + מיני מנוי פלייאוף',
+  },
+  {
+    name: 'm-haifa',
+    title: 'מכבי חיפה - מכבי נתניה',
+    date: new Date(2021, 9, 25)
+  },
+  {
+    name: 'h-b7',
+    title: 'מכבי נתניה - הפועל באר שבע',
+    date: new Date(2021, 9, 30)
+  },
+  {
+    name: 'm-pt',
+    title: 'מכבי נתניה - מכבי פ״ת',
+    date: new Date(2021, 10, 27)
+  },
+  {
+    name: 'ashdod',
+    title: 'מכבי נתניה - מ.ס אשדוד',
+    date: new Date(2021, 10, 30)
+  },
+  {
+    name: 'h-tlv',
+    title: 'מכבי נתניה - הפועל ת"א',
+    date: new Date(2021, 11, 11)
+  },
+  {
+    name: 'h-haifa',
+    title: 'מכבי נתניה - הפועל חיפה',
+    date: new Date(2021, 11, 21)
+  },
+  {
+    name: 'b-jslm',
+    title: 'מכבי נתניה - בית"ר ירושלים',
+    date: new Date(2022, 0, 1)
+  },
+  {
+    name: 'h-hadera',
+    title: 'הפועל חדרה - מכבי נתניה',
+    date: new Date(2022, 0, 9)
+  },
+  {
+    name: 'h-b7-cup',
+    title: 'מכבי נתניה - הפועל באר שבע (גביע)',
+    date: new Date(2022, 0, 12)
+  },
+  {
+    name: 'sakhnin',
+    title: 'מכבי נתניה - בני סכנין',
+    date: new Date(2022, 0, 17),
+  },
+  {
+    name: 'm-tlv',
+    title: 'מכבי ת״א - מכבי נתניה',
+    date: new Date(2022, 0, 22),
+  },
+  {
+    name: 'm-haifa-h',
+    title: 'מכבי נתניה - מכבי חיפה',
+    date: new Date(2022, 0, 30),
+  },
+  {
+    name: 'h-b7-away',
+    title: 'הפועל ב"ש - מכבי נתניה',
+    date: new Date(2022, 1, 5),
+  },
+  {
+    name: 'h-jslm',
+    title: 'מכבי נתניה - הפועל ירושלים',
+    date: new Date(2022, 1, 12),
+  },
+  {
+    name: 'm-pt-away',
+    title: 'מכבי נתניה - מכבי פ״ת',
+    date: new Date(2022, 1, 19, 15, 0, 0),
+  },
+  {
+    name: 'ashdod-away',
+    title: 'מ.ס אשדוד - מכבי נתניה',
+    date: new Date(2022, 1, 26, 15, 0, 0)
+  },
+  {
+    name: 'k8',
+    title: 'מכבי נתניה - עירוני קריית שמונה',
+    date: new Date(2022, 2, 5, 17, 30, 0),
+  },
+  {
+    name: 'h-tlv-away',
+    title: 'הפועל תל אביב - מכבי נתניה',
+    date: new Date(2022, 2, 12, 19, 0, 0),
+  },
+  {
+    name: 'h-b7-away-2',
+    title: 'הפועל ב"ש - מכבי נתניה',
+    date: new Date(2022, 2, 19),
+  },
+  {
+    name: 'm-tlv-2',
+    title: 'מכבי נתניה - מכבי ת״א',
+    date: new Date(2022, 3, 4),
+  },
+  {
+    name: 'sakhnin-away',
+    title: 'בני סכנין - מכבי נתניה',
+    date: new Date(2022, 3, 10),
+  },
+  {
+    name: 'h-tlv-away-2',
+    title: 'הפועל תל אביב - מכבי נתניה',
+    date: new Date(2022, 3, 18, 20, 0, 0),
+  },
+  {
+    name: 'm-haifa-h-2',
+    title: 'מכבי נתניה - מכבי חיפה',
+    date: new Date(2022, 3, 23),
+  },
+  {
+    name: 'h-b7-2',
+    title: 'מכבי נתניה - הפועל באר שבע',
+    date: new Date(2022, 4, 1)
+  },
+  {
+    name: 'm-tlv-away',
+    title: 'מכבי נתניה - מכבי ת״א',
+    date: new Date(2022, 4, 7, 19, 30, 0)
+  },
+  {
+    name: 'sakhnin-2',
+    title: 'מכבי נתניה - בני סכנין',
+    date: new Date(2022, 4, 10, 20, 30, 0),
+  },
+  {
+    name: 'h-tlv-2',
+    title: 'מכבי נתניה - הופעל ת״א',
+    date: new Date(2022, 4, 16, 20, 30, 0),
+  },
+  {
+    name: 'm-haifa-away',
+    title: 'מכבי חיפה - מכבי נתניה',
+    date: new Date(2022, 4, 21, 20, 30, 0),
   },
 ];
