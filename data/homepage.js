18d2da4caf188c6e9c2fa33541ed68e503a84f54
michael.ziso <michael.ziso@taboola.com>
2022-07-21 17:09:27 +0300

    update game

--- data/homepage.js
+++ data/homepage.js
@@ -1,7 +1,12 @@
 const data = [
-   {
-    name: 'toto-m-tlv',
-    title: 'מכבי תל אביב - מכבי נתניה',
-    date: new Date(2022, 6, 14, 20, 0, 0),
-  },
+    {
+        name: 'toto-m-tlv',
+        title: 'מכבי תל אביב - מכבי נתניה',
+        date: new Date(2022, 6, 14, 20, 0, 0),
+    },
+    {
+        name: 'conf-basaksehir',
+        title: 'מכבי נתניה - בשאקשהיר',
+        date: new Date(2022, 6, 14, 20, 0, 0),
+    },
 ];
