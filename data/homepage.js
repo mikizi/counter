8c3b6086effb067d01973c22b7ed115bb1cfb54d
michael.ziso <michael.ziso@taboola.com>
2022-07-31 21:00:02 +0300

    new subscription page

--- data/homepage.js
+++ data/homepage.js
@@ -1,5 +1,10 @@
 const data = [
     {
+        name: 'subscription',
+        title: 'מנויים',
+        date: new Date(2022, 6, 0, 0, 0, 0),
+    },
+    {
         name: 'toto-m-tlv',
         title: 'מכבי תל אביב - מכבי נתניה',
         date: new Date(2022, 6, 14, 20, 0, 0),
