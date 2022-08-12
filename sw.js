cc9d77cfa05eacd819fbb495a33ce9f521caa2ed
michael.ziso <michael.ziso@taboola.com>
2022-08-12 15:09:50 +0300

    update subs + new page b-jslm

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const VERSION = '2.1.5'
+const VERSION = '2.1.6'
 const PRECACHE = `precache-v${VERSION}`;
 const RUNTIME = `runtime-v${VERSION}`;
 
