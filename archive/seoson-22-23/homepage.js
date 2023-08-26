1ff75187a7b611bba69d0b6a82e995010e2b7116
KfirZiso <88159811+KfirZiso@users.noreply.github.com>
2023-08-26 18:26:27 +0300

    new seoson

---
+++ archive/seoson-22-23/homepage.js
@@ -0,0 +1,113 @@
+const data = [
+    {
+        name: 'subscription',
+        title: 'מנויים',
+        date: new Date(2022, 6, 0, 0, 0, 0),
+    },
+    {
+        name: 'toto-m-tlv',
+        title: 'מכבי תל אביב - מכבי נתניה',
+        date: new Date(2022, 6, 14, 20, 0, 0),
+    },
+    {
+        name: 'conf-basaksehir',
+        title: 'מכבי נתניה - בשאקשהיר',
+        date: new Date(2022, 6, 28, 19, 45, 0),
+    },
+    {
+        name: 'b-jslm-h',
+        title: 'מכבי נתניה - ביתר ירושלים',
+        date: new Date(2022, 7, 20, 20, 0, 0),
+    },
+    {
+        name: 'm-haifa-a',
+        title: 'מכבי חיפה - מכבי נתניה',
+        date: new Date(2022, 7, 27, 20, 0, 0),
+    },
+    {
+        name: 'h-tlv-h',
+        title: 'מכבי נתניה - הפועל ת״א',
+        date: new Date(2022, 8, 3, 20, 0, 0),
+    },
+    /*{
+        name: 'h-haifa-a',
+        title: 'הפועל חיפה - מכבי נתניה',
+        date: new Date(2022, 8  , 17, 20, 0, 0),
+    },*/
+    {
+        name: 'h-b7-h',
+        title: 'מכבי נתניה - הפועל באר שבע',
+        date: new Date(2022, 9, 1, 20, 0, 0),
+
+    },
+    {
+        name: 'h-jslm-h',
+        title: 'מכבי נתניה - הפועל ירושלים',
+        date: new Date(2022, 9, 17, 19, 30, 0),
+    },
+    {
+        name: 'm-tlv-a',
+        title: 'מכבי ת"א - מכבי נתניה',
+        date: new Date(2022, 9, 24, 20, 30, 0),
+    },
+    {
+        name: 'h-hadera-a',
+        title: 'הפועל חדרה - מכבי נתניה',
+        date: new Date(2022, 10, 5, 17, 30, 0),
+    },
+    {
+        name: 'k8-h',
+        title: 'מכבי נתניה - הפועל ק"ש',
+        date: new Date(2022, 10, 8, 20, 0, 0),
+    },
+    {
+        name: 'ashdod-a',
+        title: 'מ.ס אשדוד - מכבי נתניה',
+        date: new Date(2022, 10, 12, 15, 0, 0),
+    },
+    {
+        name: 'sakhnin-h',
+        title: 'מכבי נתניה - בני סכנין',
+        date: new Date(2023, 0, 7, 15, 0, 0),
+    },
+    {
+        name: 'toto-final',
+        title: 'מכבי נתניה - הפועל ב"ש',
+        date: new Date(2023, 0, 18, 20, 0, 0),
+
+    },
+    {
+        name: 'nes-ziona-h',
+        title: 'מכבי נתניה - סקציה נס ציונה',
+        date: new Date(2023, 0, 28, 18, 0, 0),
+    },
+    {
+        name: 'm-haifa-cup-h',
+        title: 'מכבי נתניה - מכבי חיפה',
+        date: new Date(2023, 0, 31, 20, 30, 0),
+    },
+    {
+        name: 'm-tlv-h',
+        title: 'מכבי נתניה - מכבי ת"א',
+        date: new Date(2023, 1, 13, 20, 30, 0),
+    },
+    {
+        name: 'h-hadera-h',
+        title: 'מכבי נתניה - הפועל חדרה',
+        date: new Date(2023, 1, 25, 19, 0, 0),
+    },
+    {
+        name: 'm-haifa-cup-a',
+        title: 'מכבי חיפה - מכבי נתניה',
+        date: new Date(2023, 1, 28, 20, 30, 0),
+    },{
+        name: 'ashdod-h',
+        title: 'מכבי נתניה - מ.ס.ע. אשדוד',
+        date: new Date(2023, 2, 11, 19, 30, 0),
+    },
+    {
+        name: 'ashdod-cup',
+        title: 'מ.ס אשדוד - מכבי נתניה (חצי גמר גביע)',
+        date: new Date(2023, 4, 2, 20, 30, 0),
+    }
+];
