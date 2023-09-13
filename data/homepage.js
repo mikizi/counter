ef23fcd3fe0ad00432a314e083773285fb799158
KfirZiso <88159811+KfirZiso@users.noreply.github.com>
2023-09-13 20:42:42 +0300

    Update - Data

--- data/homepage.js
+++ data/homepage.js
@@ -3,5 +3,10 @@ const data = [
         name: 'bney-reyna-h',
         title: 'מכבי נתניה - מכבי בני ריינה',
         date: new Date(2023, 7, 28, 20, 0, 0),
+    },
+    {
+        name: 'hapoel-p"t-h',
+        title: 'מכבי נתניה - הפועל פתח תקווה',
+        date: new Date(2023, 8, 17, 20, 0, 0),
     }
 ];
