051aed7453be0bffc778363fcf1603e413d75744
KfirZiso <88159811+KfirZiso@users.noreply.github.com>
2024-05-01 20:21:22 +0300

    Up vs Hapoel

--- data/homepage.js
+++ data/homepage.js
@@ -73,6 +73,11 @@ const data = [
         name: 'h-jeruslam-h-po',
         title: 'מכבי נתניה - הפועל ירושלים',
         date: new Date(2024,3,20)
+    },
+    {
+        name: 'h-ta-h-po',
+        title: 'מכבי נתניה - הפועל ירושלים',
+        date: new Data(2024,4,4)
     }
 
 ];
